b6df9b154c141db04355cd748cd94263e06a9984
Sahil-4 <www.sahil.875020@gmail.com>
2024-01-25 19:13:24 +0530

    completed profile component UI part

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
 
 function Profile({ logOut }) {
+  const [view, setView] = useState("close");
+
+  const handleOpen = () => {
+    setView("open");
+    document.addEventListener("click", handleClose, true);
+  };
+
+  const handleClose = () => {
+    setView("close");
+    document.removeEventListener("click", handleClose, true);
+  };
+
   return (
-    <div className="profile">
-      <h3 className="text pointer">Your Name</h3>
-      <button className="text pointer" onClick={logOut}>
-        Deactivate Account
-      </button>
-      <button className="text pointer" onClick={logOut}>
-        Log out
-      </button>
-    </div>
+    <>
+      <div
+        className={`pointer profile profile-close ${
+          view === "close" ? "" : "hidden"
+        }`}
+        onClick={handleOpen}
+      >
+        A
+      </div>
+      <div
+        className={`profile ${view === "close" ? "hidden" : ""}`}
+        onClick={handleClose}
+      >
+        <h3 className="text pointer">Your Name</h3>
+        <button className="text pointer" onClick={logOut}>
+          Deactivate Account
+        </button>
+        <button className="text pointer" onClick={logOut}>
+          Log out
+        </button>
+      </div>
+    </>
   );
 }
 
