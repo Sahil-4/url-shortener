9d5cdcd1e93ca58c5cfafa2afa520db5ad5261c5
Sahil-4 <www.sahil.875020@gmail.com>
2024-01-27 19:37:00 +0530

    created URLShortenerBox component

--- src/Components/URLShortenerBox.jsx
+++ src/Components/URLShortenerBox.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 
 function URLShortenerBox() {
+  const [url, setUrl] = useState("");
+
+  const shortURL = () => {
+    if (url === "") return alert("Add a valid URL");
+    console.log(url);
+  };
+
+  const copyURL = () => {
+    if (url === "") return alert("Add a valid URL");
+    console.log(url);
+  };
+
   return (
-    <div>URLShortenerBox</div>
-  )
+    <div>
+      <div className="url-shortener-box">
+        <input
+          type="text"
+          name="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <div>
+          <button className="pointer" onClick={shortURL}>
+            Short
+          </button>
+          <button className="pointer" onClick={copyURL}>
+            Copy
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default URLShortenerBox
+export default URLShortenerBox;
