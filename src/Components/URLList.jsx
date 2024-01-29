1087e1f9d9e316133863a3cd673904eb7fa9807d
Sahil-4 <www.sahil.875020@gmail.com>
2024-01-29 19:02:15 +0530

    created List and URL component

--- src/Components/URLList.jsx
+++ src/Components/URLList.jsx
@@ -1,8 +1,22 @@
+import URLBox from "./URLBox";
 
 function URLList() {
+  const urls = [
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+    "http://short-url.com/redirect/to/mypage", "http://short-url.com/redirect/to/mypage",
+  ];
+
   return (
-    <div>URLList</div>
-  )
+    <div className="container list">
+      {urls.map((url) => (
+        <URLBox key={url} url={url} />
+      ))}
+    </div>
+  );
 }
 
-export default URLList
+export default URLList;
