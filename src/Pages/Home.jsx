9695609bcd834665900e2d7a120f534c74367f62
Sahil-4 <www.sahil.875020@gmail.com>
2024-04-07 01:20:58 +0530

    added user authentication redirect

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,25 @@
-import Navbar from "../Components/Navbar"
-import URLList from "../Components/URLList"
-import URLShortenerBox from "../Components/URLShortenerBox"
+import Navbar from "../Components/Navbar";
+import URLList from "../Components/URLList";
+import URLShortenerBox from "../Components/URLShortenerBox";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function Home() {
+  const userState = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userState.profile) navigate("/auth");
+  }, [navigate, userState.profile]);
+
   return (
     <>
-     <Navbar />
-     <URLShortenerBox />
-     <URLList />
+      <Navbar />
+      <URLShortenerBox />
+      <URLList />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
