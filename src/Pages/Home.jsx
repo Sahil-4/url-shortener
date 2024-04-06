b59f17f6a67009360c7059a98549f919f3f06f75
Sahil-4 <www.sahil.875020@gmail.com>
2024-04-07 01:57:54 +0530

    added code to varify token expiry and log out user

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,16 +2,21 @@ import Navbar from "../Components/Navbar";
 import URLList from "../Components/URLList";
 import URLShortenerBox from "../Components/URLShortenerBox";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { logout } from "../Redux/features/authSlice";
 
 function Home() {
+  const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!userState.profile) navigate("/auth");
-  }, [navigate, userState.profile]);
+    const token = userState.profile?.token;
+    if (!userState.profile || !token) return navigate("/auth");
+    if (jwtDecode(token).exp < Date.now() / 1000) dispatch(logout());
+  }, [dispatch, navigate, userState.profile]);
 
   return (
     <>
